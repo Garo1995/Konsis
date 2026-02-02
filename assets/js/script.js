c16255ddf84b77200180d29863a305a27fe882ee
Garik_18 <garikarshakyam27@gmail.com>
2026-02-02 18:14:47 +0300

    addd

--- assets/js/script.js
+++ assets/js/script.js
@@ -286,3 +286,62 @@ document.querySelectorAll('.languages-row').forEach(row => {
     track.style.animationDuration = speed + 's';
     track.style.animationDirection = direction === 'left' ? 'normal' : 'reverse';
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('.open_modal').on('click', function () {
+    let attr = $(this).attr('data-val');
+    let modal = $('#' + attr);
+    modal.removeClass('out');
+    modal.fadeIn();
+    $('body').addClass('body_fix');
+});
+
+$('.close').on('click', function () {
+
+    $('body').removeClass('body_fix');
+    let prt = $(this).parents('.modal');
+
+    prt.addClass('out')
+    setTimeout(function () {
+        prt.fadeOut();
+    }, 100);
+});
+
+$(window).on('click', function (event) {
+    $('.modal').each(function () {
+        let gtattr = $(this).attr('id');
+        let new_mod = $('#' + gtattr);
+        let md_cnt = $(new_mod).find('.modal-content');
+        if (event.target === $(md_cnt)[0]) {
+            setTimeout(function () {
+                $(new_mod).addClass('out');
+                $(new_mod).fadeOut()
+            }, 100)
+            $('body').removeClass('body_fix');
+        }
+        if (event.target === this) {
+            setTimeout(function () {
+                $(new_mod).addClass('out');
+                $(new_mod).fadeOut()
+            }, 100)
+        }
+    })
+});
+
+
